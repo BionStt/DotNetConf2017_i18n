24fda45c0ef31218b68ba87f615262b59d6692a8
Chris Woodruff <cwoodruff@live.com>
2017-09-18 12:53:13 -0400

    Added the globalization demo and base project for the Chinook Music Store

---
+++ Culture/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
+
+namespace Culture
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Add the needed cultures to the list
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("es-ES"),
+                new CultureInfo("de-DE"),
+                new CultureInfo("fr-FR"),
+                new CultureInfo("sv-SE")
+            };
+
+            // Instaliate the RequestLocalizationOptions w/ cultures
+            var options = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            // Set the Localization Middleware
+            app.UseRequestLocalization(options);
+
+            // Tap into the app's request pipeline to output culture details
+            app.Run(async (context) =>
+            {
+                context.Response.StatusCode = 200;
+                context.Response.ContentType = "text/html; charset=utf-8";
+
+                var detectedCultureName = CultureInfo.CurrentCulture.EnglishName;
+                var detectedUiCultureName = CultureInfo.CurrentUICulture.EnglishName;
+
+                var cultureTable =  "<html><body>"
+                       + "<table border=\"1\" cellpadding=\"5\" style=\"border-collapse:collapse;\">"
+                       + $"<tr><td>Detected Culture</td><td>{detectedCultureName}</td></tr>"
+                       + $"<tr><td>Detected UI Culture</td><td>{detectedUiCultureName}</td></tr>"
+                       + $"<tr><td>Today's Date</td><td>{DateTime.Now:D}</td></tr>"
+                       + $"<tr><td>Culture's Formatted Number</td><td>{(1234567.89):n}</td></tr>"
+                       + $"<tr><td>Culture's Currency</td><td>{(42):C}</td></tr>"
+                       + "</table></body></html>";
+
+                await context.Response.WriteAsync(cultureTable);
+            });
+        }
+    }
+}
